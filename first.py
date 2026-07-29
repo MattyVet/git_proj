973ee15c694abe4ec00b17ba0e156eb8ab2dac86
first <first@gmail.com>
2026-07-29 12:46:47 +0500

    add date of birthday
    added age display

--- first.py
+++ first.py
@@ -1,5 +1,23 @@
+from datetime import datetime
+
 name = input('Enter you name: ')
 
-n = int(input('Enter number: '))
+while True:
+    date_str = input('Enter your date of birthday (dd.mm.YYYY): ')
+    try:
+        birth_date = datetime.striptime(date_str, "%d.%m.%Y")
+        break
+    except ValueError:
+        print('Error: Invalid date format, using dd.mm.YYYY')
+
+today = datetime.today()
+
+age = today.year - birth_date.year
+if (today.month, today.day) < (birth_date.month, birth_date.day):
+    age -= 1
+
+n = int(input('Enter the number of greetings: '))
 
 [print(f"Hello {name}!") for _ in range(n)]
+
+print(['Your age: {age}', f'Happy birthday!!!\nNow your age: {age}'][(today.month, today.day) == (birth_date.month, birth_date.day)])
